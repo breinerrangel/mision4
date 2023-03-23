debb4b9689e08b12b16d39e55598727d7f8786a0
breiner <rangelbreiner19@gmail.com>
2023-03-22 21:58:09 -0500

    nuevo

--- js/formulario.js
+++ js/formulario.js
@@ -15,12 +15,12 @@ class personas{
         this.direccion=direccion
         this.area=area
     }
-}
+ }
+
+
 
 const registro=[]
-
-
-
+let informacion=[]
 let nombre=document.getElementById("nombre")
 let apellido=document.getElementById("apellido")
 let correo=document.getElementById("email")
@@ -29,22 +29,53 @@ let direccion=document.getElementById("direccion")
 let usuario=document.getElementById("usuario")
 let enviar=document.getElementById("enviar")
 let area=document.getElementById("area")
-enviar.addEventListener("click",()=>{
-    
+enviar.addEventListener("click",()=>{ 
+    const añadidos=new personas(nombre.value,edad.value,correo.value,usuario.value,apellido.value,direccion.value,area.value)
     
     if(registro.find(Element=>Element.usuario==usuario.value)){
         alert("usuario ya existente")
         
     }
-    else{
-    const añadidos=new personas(nombre.value,edad.value,correo.value,usuario.value,apellido.value,direccion.value,area.value)
-    registro.push(añadidos)
-    localStorage.setItem("datos",JSON.stringify(registro))
-    console.log(registro)
+    else{ 
+        registro.push(añadidos)
+        if(localStorage.getItem("dato")==null){
+            
+            console.log("almacenamiento local esta vacio")
+          
+           
+        }
+        else{
+            informacion=JSON.parse(localStorage.getItem("dato")) 
     
+           
+
+        }
+        informacion.push({nombre:nombre.value,edad:edad.value,correo:correo.value,usuario:usuario.value,apellido:apellido.value,direccion:direccion.value,area:area.value
+      
+        })
+        localStorage.setItem("dato",JSON.stringify(informacion))
+        location.href="index.html"
+    
+         
+   
     }
+     
+   
+   
+
+   
+    
+   
+    
+    
+   
+    
+})
     
     
     
 
-})
+
+
+
+
--- js/main.js
+++ js/main.js
@@ -1,15 +1,81 @@
- 
-const datos=JSON.parse(localStorage.getItem("datos"))
-for(let i=0;i<datos.length;i++){
-    if(datos[i].area=="docencia"){
-        document.getElementById("nombre-docencia").innerHTML=`${datos[i].nombre} ${datos[i].apellido}`
+
+const datos=JSON.parse(localStorage.getItem("dato"))
+let busqueda=document.getElementById("buscar")
+let tabla=document.getElementById("tabla")
+
+
+   
+datos.forEach(element => {
+   
+    
+    
+    if(element.area=="docencia"){
+        let div1=document.createElement("div")
+        div1.textContent=`${element.nombre} ${element.apellido}`
+        document.getElementById("nombre-docencia").append(div1)
+        let div2=document.createElement("div")
+        div2.textContent=`${element.usuario}`
+        document.getElementById("usuario-docencia").append(div2)
+        let div3=document.createElement("div")
+        div3.textContent=`${element.correo}`
+        document.getElementById("correo-docencia").append(div3)
+        let div4=document.createElement("div")
+        div4.textContent=`${element.edad}`
+        document.getElementById("edad-docencia").append(div4)
+        
+        
+
+       
+
+
 
     }
-    else if(datos[i].area=="tecnologia"){
-        document.getElementById("").innerHTML=`${datos[i].nombre} ${datos[i].apellido}`
+    else if(element.area=="tecnologia"){
+        let div1=document.createElement("div")
+        div1.textContent=`${element.nombre} ${element.apellido}`
+        document.getElementById("nombre-tecnologia").append(div1)
+        let div2=document.createElement("div")
+        div2.textContent=`${element.usuario}`
+        document.getElementById("usuario-tecnologia").append(div2)
+        let div3=document.createElement("div")
+        div3.textContent=`${element.correo}`
+        document.getElementById("email-tecnologia").append(div3)
+        let div4=document.createElement("div")
+        div4.textContent=`${element.edad}`
+        document.getElementById("edad-tecnologia").append(div4)
+        
+        
+    }
+    else if(element.area=="Marketing"){
+        let div1=document.createElement("div")
+        div1.textContent=`${element.nombre} ${element.apellido}`
+        document.getElementById("nombre.Marketing").append(div1)
+        let div2=document.createElement("div")
+        div2.textContent=`${element.usuario}`
+        document.getElementById("usuario.Marketing").append(div2)
+        let div3=document.createElement("div")
+        div3.textContent=`${element.correo}`
+        document.getElementById("email.Marketing").append(div3)
+        let div4=document.createElement("div")
+        div4.textContent=`${element.edad}`
+        document.getElementById("edad.Marketing").append(div4)
+        
 
     }
-}
+})
+
+busqueda.addEventListener("keyup",e=>{
+    
+       
+        
+    
+
+})
+
+
+
+    
+
 
 
 
